c0cd1730857ce6587c465617f08aaa8745056cbf
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-02-21 22:33:10 +0900

    9.3.1 classnames
    9.3.1 classnames

--- styling-react/src/CSSModule.js
+++ styling-react/src/CSSModule.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import classNames from 'classnames/bind';
 import styles from './CSSModule.module.css';
+
+const cx = classNames.bind(styles);
+
 const CSSModule = () => {
     return (
-        <div className={`${styles.wrapper} ${styles.inverted}`}>
+        // <div className={`${styles.wrapper} ${styles.inverted}`}>
+        // 9.3.1 classnames
+        <div className={cx('wrapper', 'inverted')}>
             안녕하세요, 저는 <span className='something'>CSS Module!</span>
         </div>
     );
