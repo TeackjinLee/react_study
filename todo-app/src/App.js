107c0ab11ae0ec42585761f56c1f553669935019
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-02-23 23:27:09 +0900

    10.2.2 TodoInsert 만들기
    10.2.2 TodoInsert 만들기

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import TodoTemplate from './components/TodoTemplate';
+import TodoInsert from './components/TodoInsert';
 
 // 10.1.4 App 컴포넌트 초기화
 const App = () => {
-  return <TodoTemplate>Todo App을 만들자!</TodoTemplate>;
+  return (
+    <TodoTemplate>
+      <TodoInsert />
+    </TodoTemplate>
+  );
 };
 
 export default App;
