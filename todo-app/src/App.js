cf9705990598e86f570aa26f57a07bb446736956
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-02-24 08:59:55 +0900

    10.2.3 TodoListItem과 TodoList 만들기 완료
    10.2.3 TodoListItem과 TodoList 만들기 완료

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
 
 // 10.1.4 App 컴포넌트 초기화
 const App = () => {
   return (
     <TodoTemplate>
       <TodoInsert />
+      <TodoList />
     </TodoTemplate>
   );
 };
--- todo-app/src/components/TodoListItem.js
+++ todo-app/src/components/TodoListItem.js
@@ -7,5 +7,17 @@ import {
 import './TodoListItem.scss';
 
 const TodoListItem = () => {
-  return <div className="TodoListItem"></div>;
+  return (
+    <div className="TodoListItem">
+      <div className="checkbox">
+        <MdCheckBoxOutlineBlank />
+        <div className="text">할일</div>
+      </div>
+      <div className="remove">
+        <MdRemoveCircleOutline />
+      </div>
+    </div>
+  );
 };
+
+export default TodoListItem;
