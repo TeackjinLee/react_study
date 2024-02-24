6b8c3feb7422f026df0ad74dc13f89fd2ffe4dd4
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-02-24 22:40:55 +0900

    10.3.2.3 todos 배열에 새 객체 추가하기
    10.3.2.3 todos 배열에 새 객체 추가하기

--- todo-app/src/components/TodoInsert.js
+++ todo-app/src/components/TodoInsert.js
@@ -3,15 +3,28 @@ import React, { useState, useCallback } from 'react';
 import { MdAdd } from 'react-icons/md';
 import './TodoInsert.scss';
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
   const [value, setvalue] = useState('');
 
   const onChange = useCallback((e) => {
     setvalue(e.target.value);
   }, []);
 
+  // 10.3.2.3 todos 배열에 새 객체 추가하기
+  const onSubmit = useCallback(
+    (e) => {
+      onInsert(value);
+      setvalue('');
+
+      // submit 이벤트는 브라우저에서 새로고침을 발생시킵니다.
+      // 이를 방지하기 위해 이함수를 호출합니다.
+      e.preventDefault();
+    },
+    [onInsert, value],
+  );
+
   return (
-    <form className="TodoInsert">
+    <form className="TodoInsert" onSubmit={onSubmit}>
       <input
         placeholder="할 일을 입력하세요"
         value={value}
