45c8982bb1590f10afbfdc5b151d5adcbb2eec82
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-02-25 22:38:29 +0900

    11.1 많은 데이터 렌더링하기
    11.1 많은 데이터 렌더링하기

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -3,29 +3,43 @@ import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
+// 11.1 많은 데이터 렌더링하기
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i < 2500; i++) {
+    array.push({
+      id: i,
+      text: `할 일 ${i}`,
+      checked: false,
+    });
+  }
+  return array;
+}
+
 // 10.1.4 App 컴포넌트 초기화
 const App = () => {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: `리액트의 기초 알아보기`,
-      checked: true,
-    },
-    {
-      id: 2,
-      text: `컴포넌트 스타일링해 보기`,
-      checked: true,
-    },
-    {
-      id: 3,
-      text: `일정 관리 앱 만들어 보기`,
-      checked: false,
-    },
-  ]);
+  // const [todos, setTodos] = useState([
+  //   {
+  //     id: 1,
+  //     text: `리액트의 기초 알아보기`,
+  //     checked: true,
+  //   },
+  //   {
+  //     id: 2,
+  //     text: `컴포넌트 스타일링해 보기`,
+  //     checked: true,
+  //   },
+  //   {
+  //     id: 3,
+  //     text: `일정 관리 앱 만들어 보기`,
+  //     checked: false,
+  //   },
+  // ]);
+  const [todos, setTodos] = useState(createBulkTodos);
 
   // 고유값으로 사용될 id
   // ref를 사용하여 변수 담기
-  const nextId = useRef(4);
+  const nextId = useRef(2501);
 
   const onInsert = useCallback(
     (text) => {
