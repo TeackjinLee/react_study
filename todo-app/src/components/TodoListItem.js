8810f74ba74d326cc9c94ab79202253aa0c30a6f
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-02-25 22:53:41 +0900

    11.4 React.memo를 사용하여 컴포넌트 성능 최적화
    컴포넌트의 props가 바꾸지 않았다면, 리랜더링하지 않도록 설정하여 함수형 컴포넌트의 리렌더링 성능을 최적화해 줄 수 있습니다.

--- todo-app/src/components/TodoListItem.js
+++ todo-app/src/components/TodoListItem.js
@@ -23,4 +23,10 @@ const TodoListItem = ({ todo, onRemove, onToggle }) => {
   );
 };
 
-export default TodoListItem;
+/****************************************
+ * React.memo를 사용하여 컴포넌트 성능 최적화
+ * 컴포넌트의 props가 바꾸지 않았다면, 리랜더링하지
+ * 않도록 설정하여 함수형 컴포넌트의 리렌더링 성능을
+ * 최적화해 줄 수 있습니다.
+ *****************************************/
+export default React.map(TodoListItem);
