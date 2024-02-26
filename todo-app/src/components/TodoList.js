d98fb8cdd39b076eb7edaea249902e36e28ee0fc
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-02-26 22:15:43 +0900

    11.8.2 TodoList 수정
    11.8.2 TodoList 수정

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,20 +1,47 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
+import { List } from 'react-virtualized';
 
 const TodoList = ({ todos, onRemove, onToggle }) => {
-  return (
-    <div className="TodoList">
-      {todos.map((todo) => (
+  const rowRenderer = useCallback(
+    ({ index, key, style }) => {
+      const todo = todos[index];
+      console.log(todo);
+      return (
         <TodoListItem
           todo={todo}
-          key={todo.id}
+          key={key}
           onRemove={onRemove}
           onToggle={onToggle}
+          style={style}
         />
-      ))}
-    </div>
+      );
+    },
+    [onRemove, onToggle, todos],
+  );
+  return (
+    // <div className="TodoList">
+    //   {todos.map((todo) => (
+    //     <TodoListItem
+    //       todo={todo}
+    //       key={todo.id}
+    //       onRemove={onRemove}
+    //       onToggle={onToggle}
+    //     />
+    //   ))}
+    // </div>
+    <List
+      className="TodoList"
+      width={512} // 전체 크기
+      height={513} // 전체 높이
+      rowCount={todos.length} // 항목개수
+      rowHeight={57} // 항목 높이
+      rowRenderer={rowRenderer}
+      list={todos} // 배열
+      style={{ outline: 'none' }} // List에 기본 적용되는 outline 스타일 제거
+    />
   );
 };
-
+// 11.7 TodoList 컴포넌트 최적화하기
 export default React.memo(TodoList);
