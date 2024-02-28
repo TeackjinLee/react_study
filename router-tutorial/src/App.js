fcdf4147b19670eb94bec470e2331733231f8d58
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-02-28 23:12:34 +0900

    13.2.4 Route 컴포넌트로 특정 주소에 컴포넌트 연결
    13.2.4 Route 컴포넌트로 특정 주소에 컴포넌트 연결

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import About from "./About";
+import Home from "./Home";
+// 13.2.4 Route 컴포넌트로 특정 주소에 컴포넌트 연결
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
