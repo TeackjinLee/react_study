cdb23d204d18711b84bc6ce88829ae8e233a0c5b
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-02-28 22:19:11 +0900

    13.2.3 페이지 만들기
    13.2.3 페이지 만들기

---
+++ router-tutorial/src/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+// 13.2.3 페이지 만들기
+const About = () => {
+  return (
+    <div>
+      <h1>소개</h1>
+      <p>이 프로젝트는 리액트 라우터 기초를 실습해 보는 예제 프로젝트입니다.</p>
+    </div>
+  );
+};
+
+export default About;
