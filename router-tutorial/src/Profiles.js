f025970013afb71008640519cd8406423686c6c3
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-03-02 23:25:47 +0900

    13.6.4 NavLink
    13.6.4 NavLink

--- router-tutorial/src/Profiles.js
+++ router-tutorial/src/Profiles.js
@@ -1,17 +1,36 @@
 import React from "react";
-import { Link, Routes, Route } from "react-router-dom";
+import { NavLink, Routes, Route } from "react-router-dom";
 import Profile from "./Profile";
 //13.5 서브 라우트
 const Profiles = () => {
+  // 13.6.4 NavLink
+  const activeStyle = {
+    background: "black",
+    color: "white",
+  };
   return (
     <div>
       <h3>사용자 목록:</h3>
       <ul>
         <li>
-          <Link to="/profiles/velopert">velopert</Link>
+          <NavLink
+            to="/profiles/velopert"
+            style={({ isActive }) => {
+              return isActive ? activeStyle : {};
+            }}
+          >
+            velopert
+          </NavLink>
         </li>
         <li>
-          <Link to="/profiles/gildong">gildong</Link>
+          <NavLink
+            to="/profiles/gildong"
+            style={({ isActive }) => {
+              return isActive ? activeStyle : {};
+            }}
+          >
+            gildong
+          </NavLink>
         </li>
       </ul>
       <Routes>
