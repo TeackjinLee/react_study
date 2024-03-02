eb464fb2dfc1394c2a3f91c9f609bcd407cc7921
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-03-02 22:43:29 +0900

    13.6.3 Switch
    react6 Switch -> Rates로 변경

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes, Link } from "react-router-dom";
+import { Route, Routes, Link, useLocation } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
 import Profiles from "./Profiles";
@@ -7,6 +7,7 @@ import HistorySample from "./HistorySample";
 
 // 13.2.4 Route 컴포넌트로 특정 주소에 컴포넌트 연결
 const App = () => {
+  const { pathname } = useLocation(); // 추가
   return (
     // 13.2.5 Link 컴포넌트를 사용하여 다른 주소로 이동하기
     <div>
@@ -32,6 +33,10 @@ const App = () => {
         {/* <Route path="/profile/:username" element={<Profile />} /> */}
         <Route path="/profiles/*" element={<Profiles />} />
         <Route path="/history" element={<HistorySample />} />
+        <Route
+          path="/*"
+          element={<h1>이 페이지는 존재하지 않습니다. - {pathname}</h1>}
+        />
       </Routes>
     </div>
   );
