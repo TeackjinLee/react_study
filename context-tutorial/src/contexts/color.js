ff36cdf4144d569748e5ab623c5eaecfab1abf04
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-03-06 22:48:04 +0900

    15.3.1 Context 파일 수정하기
    15.3.1 Context 파일 수정하기

--- context-tutorial/src/contexts/color.js
+++ context-tutorial/src/contexts/color.js
@@ -1,5 +1,33 @@
-import { createContext } from "react";
+import React, { createContext, useState } from "react";
 // 15.2.1 새 Context 만들기
-const ColorContext = createContext({ color: "black" });
+// const ColorContext = createContext({ color: "black" });
+
+// 15.3.1 Context 파일 수정하기
+const ColorContext = createContext({
+  sate: { color: "black", subcolor: "red" },
+  actions: {
+    setColor: () => {},
+    setSubColor: () => {},
+  },
+});
+
+const ColorProvider = ({ children }) => {
+  const [color, setColor] = useState("black");
+  const [subcolor, setSubColor] = useState("red");
+
+  const value = {
+    state: { color, subcolor },
+    actions: { setColor, setSubColor },
+  };
+  return (
+    <ColorContext.Provider value={value}>{children}</ColorContext.Provider>
+  );
+};
+
+// const ColorConsumer = ColorContext.Consumer와 같은 의미
+const { Consumer: ColorConsumer } = ColorContext;
+
+// ColorProvider와 ColorConsumer 내보내기
+export { ColorProvider, ColorConsumer };
 
 export default ColorContext;
