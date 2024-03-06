01ed60596d8dd0ddc69aa79d4ce0094ff7fbad6c
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-03-06 23:41:06 +0900

    15.4.1 useContext Hook 사용하기
    15.4.1 useContext Hook 사용하기

--- context-tutorial/src/components/ColorBox.js
+++ context-tutorial/src/components/ColorBox.js
@@ -1,8 +1,9 @@
-import React from "react";
-// import ColorContext from "../contexts/color";
-import { ColorConsumer } from "../contexts/color";
+import React, { useContext } from "react";
+import ColorContext from "../contexts/color";
+// import { ColorConsumer } from "../contexts/color";
 // 15.2.2 Consumer 사용하기
 const ColorBox = () => {
+  const { state } = useContext(ColorContext);
   return (
     // <ColorContext.Consumer>
     //   {(value) => (
@@ -16,26 +17,43 @@ const ColorBox = () => {
     //   )}
     // </ColorContext.Consumer>
     // 15.3.2 새로워진 Context를 프로젝트에 반영하기
-    <ColorConsumer>
-      {({ state }) => (
-        <>
-          <div
-            style={{
-              width: "64px",
-              height: "64px",
-              background: state.color,
-            }}
-          />
-          <div
-            style={{
-              width: "32px",
-              height: "32px",
-              background: state.subcolor,
-            }}
-          />
-        </>
-      )}
-    </ColorConsumer>
+    // <ColorConsumer>
+    //   {({ state }) => (
+    //     <>
+    //       <div
+    //         style={{
+    //           width: "64px",
+    //           height: "64px",
+    //           background: state.color,
+    //         }}
+    //       />
+    //       <div
+    //         style={{
+    //           width: "32px",
+    //           height: "32px",
+    //           background: state.subcolor,
+    //         }}
+    //       />
+    //     </>
+    //   )}
+    // </ColorConsumer>
+    // 15.4.1 useContext Hook 사용하기
+    <>
+      <div
+        style={{
+          width: "64px",
+          height: "64px",
+          background: state.color,
+        }}
+      />
+      <div
+        style={{
+          width: "32px",
+          height: "32px",
+          background: state.subcolor,
+        }}
+      />
+    </>
   );
 };
 
