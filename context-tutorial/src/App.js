a567415d1e59173268360e6eb4d18cccb49f2aad
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-03-06 23:00:16 +0900

    15.3.2 새로워진 Context를 프로젝트에 반영하기
    15.3.2 새로워진 Context를 프로젝트에 반영하기

--- context-tutorial/src/App.js
+++ context-tutorial/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import ColorBox from "./components/ColorBox";
-import ColorContext from "./contexts/color";
+// import ColorContext from "./contexts/color";
+import { ColorProvider } from "./contexts/color";
 
 const App = () => {
   return (
@@ -8,11 +9,16 @@ const App = () => {
     //   <ColorBox />
     // </div>
     // 15.2.3 Provider
-    <ColorContext.Provider value={{ color: "red" }}>
+    // <ColorContext.Provider value={{ color: "red" }}>
+    //   <div>
+    //     <ColorBox />
+    //   </div>
+    // </ColorContext.Provider>
+    <ColorProvider>
       <div>
         <ColorBox />
       </div>
-    </ColorContext.Provider>
+    </ColorProvider>
   );
 };
 
--- context-tutorial/src/components/ColorBox.js
+++ context-tutorial/src/components/ColorBox.js
@@ -1,19 +1,41 @@
 import React from "react";
-import ColorContext from "../contexts/color";
+// import ColorContext from "../contexts/color";
+import { ColorConsumer } from "../contexts/color";
 // 15.2.2 Consumer 사용하기
 const ColorBox = () => {
   return (
-    <ColorContext.Consumer>
-      {(value) => (
-        <div
-          style={{
-            width: "64px",
-            height: "64px",
-            background: value.color,
-          }}
-        />
+    // <ColorContext.Consumer>
+    //   {(value) => (
+    //     <div
+    //       style={{
+    //         width: "64px",
+    //         height: "64px",
+    //         background: value.color,
+    //       }}
+    //     />
+    //   )}
+    // </ColorContext.Consumer>
+    // 15.3.2 새로워진 Context를 프로젝트에 반영하기
+    <ColorConsumer>
+      {({ state }) => (
+        <>
+          <div
+            style={{
+              width: "64px",
+              height: "64px",
+              background: state.color,
+            }}
+          />
+          <div
+            style={{
+              width: "32px",
+              height: "32px",
+              background: state.subcolor,
+            }}
+          />
+        </>
       )}
-    </ColorContext.Consumer>
+    </ColorConsumer>
   );
 };
 
