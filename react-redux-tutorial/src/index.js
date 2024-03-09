fb9ede0846148bb3a776f16ebfafef65f51f2fb2
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-03-09 23:41:40 +0900

    17.4.3 Redux DevTools의 설치 및 적용
    17.4.3 Redux DevTools의 설치 및 적용

--- react-redux-tutorial/src/index.js
+++ react-redux-tutorial/src/index.js
@@ -3,13 +3,14 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import './index.css';
 import App from './App';
 import rootReducer from './modules';
 
 // 17.4.2 Provider 컴포넌트를 사용하여 프로젝트에 리덕스 적용하기
-const store = createStore(rootReducer);
-
+const store = createStore(rootReducer, composeWithDevTools());
+console.log(store);
 const container = document.getElementById('root');
 const root = createRoot(container); // TypeScript 사용 시 createRoot(container!)로 적용
 root.render(
