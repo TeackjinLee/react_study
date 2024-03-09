987181b52974a5db5cd9a0e77b20ab36eadfdf27
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-03-09 11:56:33 +0900

    17.4.1 스토어 만들기
    17.4.1 스토어 만들기

--- react-redux-tutorial/src/index.js
+++ react-redux-tutorial/src/index.js
@@ -1,9 +1,14 @@
 // 17.4.1 스토어 만들기
 import React from 'react';
-import { ReactDOM } from 'react';
+import { createRoot } from 'react-dom/client';
 import { createStore } from 'redux';
 import './index.css';
 import App from './App';
 import rootReducer from './modules';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// 17.4.2 Provider 컴포넌트를 사용하여 프로젝트에 리덕스 적용하기
+const store = createStore(rootReducer);
+
+const container = document.getElementById('root');
+const root = createRoot(container); // TypeScript 사용 시 createRoot(container!)로 적용
+root.render(<App />);
