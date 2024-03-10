7ea61e6c2a655ed1ddd31699f34b4f1d92073401
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-03-10 16:21:01 +0900

    17.5.2 TodosContainer 만들기
    17.5.2 TodosContainer 만들기

--- react-redux-tutorial/src/App.js
+++ react-redux-tutorial/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 // import Counter from './compontes/Counter';
-import Todos from './compontes/Todos';
+// import Todos from './compontes/Todos';
 import CounterContainer from './compontes/CounterContainer';
+import TodosContainer from './compontes/TodosContainer';
 
 const App = () => {
   return (
     <div>
       <CounterContainer />
       <hr />
-      <Todos />
+      <TodosContainer />
     </div>
   );
 };
---
+++ react-redux-tutorial/src/compontes/TodosContainer.js
@@ -0,0 +1,40 @@
+// 17.5.2 TodosContainer 만들기
+import React from 'react';
+import { connect } from 'react-redux';
+import { changeInput, insert, toggle, remove } from '../modules/todos';
+import Todos from '../compontes/Todos';
+
+const TodosContainer = ({
+  input,
+  todos,
+  changeInput,
+  insert,
+  toggle,
+  remove,
+}) => {
+  return (
+    <Todos
+      input={input}
+      todos={todos}
+      onChangeInput={changeInput}
+      onInsert={insert}
+      onToggle={toggle}
+      onRemove={remove}
+    />
+  );
+};
+
+export default connect(
+  // 비구조 할당을 통해 todos를 분리하여
+  // state.todos.input대신 todos.input을 사용
+  ({ todos }) => ({
+    input: todos.input,
+    todos: todos.todos,
+  }),
+  {
+    changeInput,
+    insert,
+    toggle,
+    remove,
+  },
+)(TodosContainer);
