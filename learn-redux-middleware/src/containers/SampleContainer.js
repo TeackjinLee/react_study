02f6f37751d5eca8e49dc4421bbf09a6276d1d14
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-03-13 23:58:11 +0900

    18.3.1.4 웹 요청 비동기 작업 처리하기(작업중)
    18.3.1.4 웹 요청 비동기 작업 처리하기(작업중)

---
+++ learn-redux-middleware/src/containers/SampleContainer.js
@@ -0,0 +1,4 @@
+import React from "react";
+import { connect } from "react-redux";
+import Sample from "../components/Sample";
+import { getPost, getUsers } from "../modules/sample";
--- learn-redux-middleware/src/modules/index.js
+++ learn-redux-middleware/src/modules/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from "redux";
 import counter from "./counter";
+import sample from "./sample";
+
 // 18.1 작업환경 준비하기
 const rootReducer = combineReducers({
   counter,
+  sample,
 });
 
 export default rootReducer;
