947b62423bdae4bc2c333022974fa4f7193047f0
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-03-14 20:07:40 +0900

    18.3.1.5 리팩토링
    18.3.1.5 리팩토링

--- learn-redux-middleware/src/containers/SampleContainer.js
+++ learn-redux-middleware/src/containers/SampleContainer.js
@@ -13,9 +13,22 @@ const SampleContainer = ({
   loadingUsers,
 }) => {
   // 클래스 형태 컴포넌트였다면 componentDidMount
+  //   useEffect(() => {
+  //     getPost(1);
+  //     getUsers(1);
+  //   }, [getPost, getUsers]);
+
   useEffect(() => {
-    getPost(1);
-    getUsers(1);
+    // useEffect에 파라미터로 넣는 함수는 async로 할 수 없기 때문에 그내부에서 async 함수를 선언하고 호출해 줍니다.
+    const fn = async () => {
+      try {
+        await getPost(1);
+        await getUsers(1);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fn();
   }, [getPost, getUsers]);
 
   return (
@@ -29,11 +42,13 @@ const SampleContainer = ({
 };
 
 export default connect(
-  ({ sample }) => ({
+  ({ sample, loading }) => ({
     post: sample.post,
     users: sample.users,
-    loadingPost: sample.loading.GET_POST,
-    loadingUsers: sample.loading.GET_USERS,
+    // loadingPost: sample.loading.GET_POST,
+    // loadingUsers: sample.loading.GET_USERS,
+    loadingPost: loading["sample/GET_POST"],
+    loadingUsers: loading["sample/GET_USERS"],
   }),
   {
     getPost,
--- learn-redux-middleware/src/modules/index.js
+++ learn-redux-middleware/src/modules/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from "redux";
 import counter from "./counter";
 import sample from "./sample";
+import loading from "./loading";
 
 // 18.1 작업환경 준비하기
 const rootReducer = combineReducers({
   counter,
   sample,
+  loading,
 });
 
 export default rootReducer;
