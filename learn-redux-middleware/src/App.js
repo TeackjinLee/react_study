24328612adf21cfcf37ac642fe070827d35c7c0c
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-03-15 23:44:20 +0900

    18.3.2.2 비동기 카운터 만들기
    18.3.2.2 비동기 카운터 만들기

--- learn-redux-middleware/src/App.js
+++ learn-redux-middleware/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-// import CounterContainers from "./containers/CounterContainers";
-import SampleContainer from "./containers/SampleContainer";
+import CounterContainers from "./containers/CounterContainers";
+// import SampleContainer from "./containers/SampleContainer";
 
 const App = () => {
   return (
     <div>
-      {/* <CounterContainers /> */}
-      <SampleContainer />
+      <CounterContainers />
+      {/* <SampleContainer /> */}
     </div>
   );
 };
--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -4,16 +4,25 @@ import { applyMiddleware, createStore } from "redux";
 import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
-import rootReducer from "./modules";
+import rootReducer, { rootSaga } from "./modules";
 // import loggerMiddleware from "./lib/loggerMiddleware";
 import { createLogger } from "redux-logger";
 // 18.3.1.2 미들웨어 적용하기
 import { thunk } from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const logger = createLogger();
+const sagaMiddleware = createSagaMiddleware();
 // const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
 // 18.2.2 redux-logger 사용하기
-const store = createStore(rootReducer, applyMiddleware(logger, thunk));
+// const store = createStore(rootReducer, applyMiddleware(logger, thunk));
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger, thunk, sagaMiddleware))
+);
+sagaMiddleware.run(rootSaga);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
