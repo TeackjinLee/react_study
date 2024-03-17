cf83fda8ed380848a3a25af17606b20d6d0c26cb
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-03-17 22:46:30 +0900

    19.1 자바스크립트 함수 비동기 로딩
    19.1 자바스크립트 함수 비동기 로딩

--- splitting-sample/src/App.js
+++ splitting-sample/src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+// import notify from "./notify";
 
 function App() {
+  // 19.1 자바스크립트 함수 비동기 로딩
+  const onClick = () => {
+    // notify();
+    import("./notify").then((result) => result.default());
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App=logo" alt="logo" />
+        <p onClick={onClick}>Hello React!</p>
       </header>
     </div>
   );
