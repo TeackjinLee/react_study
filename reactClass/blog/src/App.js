2c7ebcb18a4a57e065070f9dce0431e4ac09668f
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-03-21 01:31:01 +0900

    React 기초 4강 : 리액트에서 버튼에 이벤트 리스너 (핸들러) 장착하는 법
    React 기초 4강 : 리액트에서 버튼에 이벤트 리스너 (핸들러) 장착하는 법

--- reactClass/blog/src/App.js
+++ reactClass/blog/src/App.js
@@ -1,7 +1,28 @@
-import logo from "./logo.svg";
+/* eslint-disable */
+
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  // useState("남자 코트 추천"); //[a,b] a=남자코트추천  b=남자코트추천 state정정해주는 함수
+  let [글제목, 글제목변경] = useState([
+    "남자코트추천",
+    "강남 우동 맛집",
+    "코린이의하루",
+  ]);
+
+  // let titleChange = () => {
+  //   글제목변경(글제목.with(0, "여자코트추천"));
+  // };
+  let titleChange = () => {
+    글제목변경(
+      글제목.map((item) => (item === "남자코트추천" ? "여자코트추천" : item))
+    );
+  };
+  console.log(글제목);
+
+  let [count, countState] = useState(0);
+
   // 데이터바인딩이 쉬워지는 React, Vue, Angular
   let posts = "강남 고기 맛집";
 
@@ -10,7 +31,29 @@ function App() {
       <div className="black-nav">
         <div style={{ color: "blue", fontSize: "30px" }}>개발 Blog</div>
       </div>
-      <h4> {posts} </h4>
+      <button onClick={titleChange}>버튼</button>
+      <div className="list">
+        <h4>
+          {" "}
+          {글제목[0]}{" "}
+          <span
+            onClick={() => {
+              countState(count + 1);
+            }}
+          >
+            ❤️
+          </span>{" "}
+          {count}
+        </h4>
+        <p>2월 17일 발행</p>
+        <hr />
+        <h4> {글제목[1]} </h4>
+        <p>2월 17일 발행</p>
+        <hr />
+        <h4> {글제목[2]} </h4>
+        <p>2월 17일 발행</p>
+        <hr />
+      </div>
     </div>
   );
 }
