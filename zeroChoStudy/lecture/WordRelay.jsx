7e58eef5a8f4a2f0a2f91dbe1bfc290554e0b2f2
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-03-25 00:01:59 +0900

    React 기본 강좌 2-8. 끝말잇기 Class 만들기(함수로 만듬)
    React 기본 강좌 2-8. 끝말잇기 Class 만들기(함수로 만듬)

--- zeroChoStudy/lecture/WordRelay.jsx
+++ zeroChoStudy/lecture/WordRelay.jsx
@@ -1,14 +1,42 @@
 const React = require('react');
-const {Component} = React;
+const {useState, useRef} = React;
 
-class WordRelay extends Component {
-    state = {
-        text: 'Hello, webpack',
+const WordRelay = () => {
+    const [word, setWord] = useState('제로초');
+    const [value, setValue] = useState('');
+    const [result, setResult] = useState('');
+    const inputRef = useRef();
+
+    const onSubmitForm = (e) => {
+        e.preventDefault();
+        console.log(word[word.length -1]);
+        if(word[word.length -1] === value[0]) {
+            setValue('');
+            setWord(value);
+            setResult('딩동댕');
+            inputRef.current.focus();
+        } else {
+            setResult('땡!!!');
+            setValue('');
+            inputRef.current.focus();
+        }
     };
 
-    render() {
-        return <h1>{this.state.text}</h1>
+    const onChangeInput = (e) => {
+        e.preventDefault();
+        setValue(e.target.value);
     }
+
+    return (
+        <>
+            <div>{word}</div>
+            <form onSubmit={onSubmitForm}>
+                <input ref={inputRef} value={value} onChange={onChangeInput}/>
+                <button>입력!</button>
+                <div>{result}</div>
+            </form>
+        </>
+    )
 }
 
 module.exports = WordRelay;
