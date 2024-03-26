12db6527a053701dc78375d9a67ecb8918c0a92c
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-03-26 22:24:28 +0900

    React 기본 강좌 3-4. 컴포넌트 분리와 props
    React 기본 강좌 3-4. 컴포넌트 분리와 props

--- zeroChoStudy/lecture/NumberBaseball.jsx
+++ zeroChoStudy/lecture/NumberBaseball.jsx
@@ -1,7 +1,46 @@
 import React, {Component} from 'react';
+import Try from './try';
 
 class NumberBaseball extends Component {
-    // test
+    state = {
+        result: '',
+        value: '',
+        // answer: getNumbers(),
+        tries: [],
+    };
+
+    onSubmitForm = () => {
+
+    };
+
+    onChangeInput = () => {
+
+    };
+
+    fruits = [
+        {fruit:'사과', taste: '맛있다'},
+        {fruit:'감', taste: '떫다'},
+        {fruit:'귤', taste: '시다'}
+    ]
+
+    render() {
+        return (
+            <>
+                <h1>{this.state.result}</h1>
+                <form onSubmit={this.onSubmit}>
+                    <input mathLength={4} value={this.state.value} onChange={this.onChangeInput} />
+                </form>
+                <div>시도 : {this.state.tries.length}</div>
+                <ul>
+                    {this.fruits.map((v, i) => {
+                        return (
+                            <Try value={v} index={i}/>
+                        );
+                    })}
+                </ul>
+            </>
+        );
+    }
 }
 
 export const hello = 'hello'; // import {hello};
