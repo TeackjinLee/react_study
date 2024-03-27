8a55341987697b0775941e0532e3d402206fdd68
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-03-27 21:51:52 +0900

    React 기본 강좌 3-13. props와 state 연결하기
    React 기본 강좌 3-13. props와 state 연결하기

--- zeroChoStudy/lecture/TryHooks.jsx
+++ zeroChoStudy/lecture/TryHooks.jsx
@@ -1,9 +1,18 @@
-import React, {memo} from 'react';
+import React, {memo, useState} from 'react';
 
 const Try = memo(({tryInfo}) => {
+    // 자식에서는 props를 변경하지 않는게 원칙 예) tryInfo = 'test'
+    // 불가피하게 자식에서 props를 변경해야 할 경우 props를 state로 만들어 작업
+    // const [result, setResult] = useState(tryInfo.result);
+
+    // const onClick = () => {
+    //     setResult('1');
+    // }
+
     return (
         <li>
             <div>{tryInfo.try}</div>
+            {/* <div onClick={onClick}>{result}</div> */}
             <div>{tryInfo.result}</div>
         </li>
     )
