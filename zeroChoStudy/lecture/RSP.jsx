f98caaa44018b6dce50e268fb8948cb5ea9e4082
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-03-30 11:46:48 +0900

    React 기본 강좌 5-3. 가위바위보 게임 만들기
    React 기본 강좌 5-3. 가위바위보 게임 만들기

--- zeroChoStudy/lecture/RSP.jsx
+++ zeroChoStudy/lecture/RSP.jsx
@@ -10,6 +10,12 @@ const rspCoords = {
     보: '-284px',
 };
 
+const computerChoice = (imgCoord) => {
+    return Object.entries(rspCoords).find(function(v) {
+        return v[1] === imgCoord;
+    })[0];
+}
+
 const scores = {
     가위: 1,
     바위: 0,
@@ -27,25 +33,7 @@ class RSP extends Component {
 
     // 컴포넌트가 첫 렌더링된 후, 여기에 비동기 요청을 많이함
     componentDidMount() {
-        this.interval =  setInterval(() => {
-            const {imgCoord} = this.state;
-            if (imgCoord === rspCoords.바위) {
-                console.log(1);
-                this.setState({
-                    imgCoord: rspCoords.가위,
-                  });
-            } else if (imgCoord === rspCoords.가위) {
-                console.log(2);
-                this.setState({
-                    imgCoord: rspCoords.보,
-                })
-            } else if (imgCoord === rspCoords.보) {
-                console.log(3);
-                this.setState({
-                    imgCoord: rspCoords.바위,
-                })
-            }
-        },1000);
+        this.interval =  setInterval(this.changeHand, 100);
     }
 
     componentDidUpdate() { // 리렌더링 후
@@ -56,8 +44,54 @@ class RSP extends Component {
         clearInterval(this.interval);
       }
     
-    onClickBtn = (choice) => {
+    changeHand =  () => {
+        const {imgCoord} = this.state;
+        if (imgCoord === rspCoords.바위) {
+            console.log(1);
+            this.setState({
+                imgCoord: rspCoords.가위,
+              });
+        } else if (imgCoord === rspCoords.가위) {
+            console.log(2);
+            this.setState({
+                imgCoord: rspCoords.보,
+            })
+        } else if (imgCoord === rspCoords.보) {
+            console.log(3);
+            this.setState({
+                imgCoord: rspCoords.바위,
+            })
+        }
+    };
 
+    onClickBtn = (choice) => {
+        const {imgCoord} = this.state;
+        clearInterval(this.interval);
+        const myScore = scores[choice];
+        const cpuScore = scores[computerChoice(imgCoord)];
+        const diff = myScore - cpuScore;
+        if (diff === 0) {
+            this.setState({
+                result: '비겼습니다!',
+            });
+        } else if ([-1, 2].includes(diff)) {
+            this.setState((prevState) => {
+                return {
+                    result: '이겼습니다!',
+                    score: prevState.score + 1,
+                }
+            })
+        } else {
+            this.setState((prevState) => {
+                return {
+                    result: '졌습니다!',
+                    score: prevState.score - 1,
+                }
+            })
+        }
+        setTimeout(() => {
+            this.interval = setInterval(this.changeHand, 100);
+        }, 2000);
     };
 
     render() {
