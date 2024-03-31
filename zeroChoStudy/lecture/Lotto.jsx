b643552b0597b3502f5afcd1356b7e6d0a6f608d
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-03-31 23:22:38 +0900

    React 기본 강좌 6-3. componentDidUpdate
    React 기본 강좌 6-3. componentDidUpdate

--- zeroChoStudy/lecture/Lotto.jsx
+++ zeroChoStudy/lecture/Lotto.jsx
@@ -23,14 +23,13 @@ class Lotto extends Component {
 
     timeouts = [];
 
-    componentDidMount() {
-        
+    runTimeouts = () => {
+        console.log('runTimeouts');
         const {winNumbers} = this.state;
         for (let i=0; i<winNumbers.length -1; i++){
             
             this.timeouts[i] = setTimeout(() => {
                 this.setState((prevState) => {
-                    console.log(1);
                     return {
                         winBalls: [...prevState.winBalls, winNumbers[i]]
                     }
@@ -43,16 +42,38 @@ class Lotto extends Component {
                 bonus: winNumbers[6],
                 redo: true,
             });
-        }, 7000)
+        }, 7000);
+    };
+    
+    componentDidMount() {
+        console.log('didMount');
+        this.runTimeouts();
     }
 
     componentWillUnmount() {
-        
         this.timeouts.forEach((v) => {
-            console.log(2);
+            
             clearTimeout(v);
         });
     }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log('didUpdate');
+        if (this.state.winBalls.length === 0) {
+            this.runTimeouts();
+        }
+    }
+
+    onClickRedo = () => {
+        console.log('onClickRedo')
+        this.setState({
+            winNumbers: getWinNumbers(),
+            winBalls: [],
+            bonus: null,
+            redo: false,
+        });
+        this.timeouts = [];
+    };
 
     render() {
         const {winBalls, bonus, redo} = this.state; 
