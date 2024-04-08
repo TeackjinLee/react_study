56be3485eb7ecdecb9a8bbdacf1326b3e439825b
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-04-08 20:20:18 +0900

    React 기본 강좌 8-2. createContext와 Provider
    React 기본 강좌 8-2. createContext와 Provider

--- zeroChoStudy/8.지뢰찾기/Form.jsx
+++ zeroChoStudy/8.지뢰찾기/Form.jsx
@@ -1,9 +1,11 @@
-import React, {useState, useCallback} from 'react';
+import React, {useState, useCallback, useContext} from 'react';
+import { TableContext } from './MineSearch';
 
 const Form = () => {
     const [row, setRow] = useState(10);
     const [cell, setCell] = useState(10);
     const [mine, setMine] = useState(10);
+    const {dispatch} = useContext(TableContext);
 
     const onChangeRow = useCallback((e) => {
         setRow(e.target.value);
@@ -18,13 +20,13 @@ const Form = () => {
     },[]);
 
     const onClickBtn = useCallback(() => {
-        
-    }, []);
+        dispatch({ type: START_GAME, row, cell, mine });
+    }, [row, cell, mine]);
 
     return (
         <div>
             <input type="number" placeholder='세로' value={row} onChange={onChangeRow} />
-            <input type="number" placeholder='가로' value={cell} onChange={onChaneCell} />
+            <input type="number" placeholder='가로' value={cell} onChange={onChangeCell} />
             <input type="number" placeholder='지뢰' value={mine} onChange={onChangeMine} />
             <button onClick={onClickBtn}></button>
         </div>
--- zeroChoStudy/8.지뢰찾기/MineSearch.jsx
+++ zeroChoStudy/8.지뢰찾기/MineSearch.jsx
@@ -1,5 +1,27 @@
-import React, {useReducer} from 'react';
+import React, { useReducer, createContext, useMemo} from 'react';
 import Table from './Table';
+
+export const CODE = {
+    MINE: -7,
+    NORMAL: -1,
+    QUESTION: -2,
+    FLAG: -3,
+    QUESTION_MINE: -4,
+    FLAG_MINE: -5,
+    CLICKED_MINE: -6,
+    OPENED: 0,
+}
+
+export const TableContext = createContext({
+    // 초기값 넣어줌
+    tableData: [
+        [-1, -1, -1, -1, -1, -1, -1],
+        [-7, -1, -1, -1, -1, -1, -1],
+        [-1, -1, -1, -1, -1, -1, -1],
+        [-1, -1, -1, -1, -1, -1, -1],
+    ],
+    dispatch: () => {},
+});
 
 const initialState = {
     tableData: [],
@@ -7,8 +29,15 @@ const initialState = {
     result: '',
 }
 
+export const START_GAME = 'START_GAME';
+
 const reducer = (state, action) => {
     switch (action.type) {
+        case START_GAME:
+            return {
+                ...state,
+                tableData: plantMine(atcion.row, action.cell, action.mine)
+            };
         default:
             return state;
     }
@@ -17,13 +46,15 @@ const reducer = (state, action) => {
 const MineSearch = () => {
     const [state, dispatch] = useReducer(reducer, initialState);
     
+    const value = useMemo(() => ({ tagbleData: state.tableData, dispatch }), [state.tableData]);
+
     return (
-        <>
+        <TableContext.Provider value={value}>
             <Form />
             <div>{state.timer}</div>
             <Table />
             <div>{state.result}</div>
-        </>
+        </TableContext.Provider>
     )
 };
 
