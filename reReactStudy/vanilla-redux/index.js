85230926b43467159327407442b26676cef6c931
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-04-13 12:45:13 +0900

    redux  vanillajavascript study complete

--- reReactStudy/vanilla-redux/index.js
+++ reReactStudy/vanilla-redux/index.js
@@ -1,1 +1,76 @@
-console.log("hello parcel");
+import { createStore } from "redux";
+
+// DOM 레퍼런스 만들기
+const divToggle = document.querySelector(".toggle");
+const counter = document.querySelector("h1");
+const btnIncrease = document.querySelector("#increase");
+const btnDecrease = document.querySelector("#decrease");
+// 액션 타입과 액션 생성 함수 정의
+const TOGGLE_SWITCH = "TOGGLE_SWITCH";
+const INCREASE = "INCREASE";
+const DECREASE = "DECREASE";
+
+const toggleSwitchAction = () => ({ type: TOGGLE_SWITCH });
+const increaseAction = (difference) => ({ type: INCREASE, difference });
+const decreaseAction = () => ({ type: DECREASE });
+
+// 초기값 설정
+const initialState = {
+  toggle: false,
+  counter: 0,
+};
+
+// 리듀서 함수 정의
+// state가 undefined일 때는 initialState를 기본값으로 사용
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case TOGGLE_SWITCH:
+      return {
+        ...state,
+        toggle: !state.toggle,
+      };
+    case INCREASE:
+      return {
+        state,
+        counter: state.counter + action.difference,
+      };
+    case DECREASE:
+      return {
+        ...state,
+        counter: state.counter - 1,
+      };
+    default:
+      return state;
+  }
+}
+// 스토어생성
+const store = createStore(reducer);
+
+// render함수 만들기
+const render = () => {
+  const state = store.getState();
+
+  if (state.toggle) {
+    divToggle.classList.add("active");
+  } else {
+    divToggle.classList.remove("active");
+  }
+
+  counter.innerText = state.counter;
+};
+
+render();
+store.subscribe(render);
+
+// 액션 발생시키기
+divToggle.onclick = () => {
+  store.dispatch(toggleSwitchAction());
+};
+
+btnIncrease.onclick = () => {
+  store.dispatch(increaseAction(1));
+};
+
+btnDecrease.onclick = () => {
+  store.dispatch(decreaseAction());
+};
