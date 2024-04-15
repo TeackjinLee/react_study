a52c58199339b4a5acae0e727d63d6d9e2800a91
이택진 <85720454+TeackjinLee@users.noreply.github.com>
2024-04-15 23:43:00 +0900

    17.리덕스를사용하여리액트애플리케이션상태관리하기
    todos 모듈에 적용하기

--- reReactStudy/react-redux-tutorial/src/modules/counter.js
+++ reReactStudy/react-redux-tutorial/src/modules/counter.js
@@ -1,29 +1,42 @@
+import { createAction, handleActions } from "redux-actions";
+
 const INCREASE = "counter/INCREASE";
 const DECREASE = "counter/DECREASE";
 
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
+// export const increase = () => ({ type: INCREASE });
+export const increase = createAction(INCREASE);
+// export const decrease = () => ({ type: DECREASE });
+export const decrease = createAction(DECREASE);
 
 const initialState = {
   number: 0,
 };
+console.log(123);
 console.log(this);
-function counter(state = initialState, action) {
-  console.log("dfs");
-  console.log(action);
+// function counter(state = initialState, action) {
+//   console.log("dfs");
+//   console.log(action);
 
-  switch (action.type) {
-    case INCREASE:
-      return {
-        number: state.number + 1,
-      };
-    case DECREASE:
-      return {
-        number: state.number - 1,
-      };
-    default:
-      return state;
-  }
-}
+//   switch (action.type) {
+//     case INCREASE:
+//       return {
+//         number: state.number + 1,
+//       };
+//     case DECREASE:
+//       return {
+//         number: state.number - 1,
+//       };
+//     default:
+//       return state;
+//   }
+// }
+
+const counter = handleActions(
+  {
+    [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+    [DECREASE]: (state, action) => ({ number: state.number - 1 }),
+  },
+  initialState
+);
 
 export default counter;
